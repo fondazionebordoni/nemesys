b5f2df514c4475c0686021ce3f27278b3546455f
paolo.bolletta <paolo.bolletta@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2010-09-16 15:49:50 +0000

    gestione soglie e variabili cablate, inserimento controllo su processi e funzioni per passaggio indirizzo MAC

--- nemesys/sysmonitor.py
+++ nemesys/sysmonitor.py
@@ -40,6 +40,18 @@ tag_proc = 'processor'
 tag_hosts = 'hostNumber'
 tag_task = 'taskList'
 tag_conn = 'activeConnections'
+tag_proc = 'processList'
+
+#soglie di sistema
+
+th_host = 1
+th_avMem = 134217728
+th_memLoad = 70
+th_wdisk = 20971520
+th_cpu = 60
+th_rdisk = 20971520
+bad_conn = [8000000, 808000000, 110000000]
+bad_proc = ['processo1', 'processo2', 'processo3']
 
 logger = logging.getLogger()
 
@@ -59,61 +71,62 @@ def getstatus(d):
 
 def checkall():
 
-  d = {tag_vers:'', tag_avMem:'', tag_wireless:'',  tag_fw:'',  tag_memLoad:'',  tag_ip:'',  tag_sys:'',  tag_wdisk:'',  tag_cpu:'',  tag_mac:'',  tag_rdisk:'',  tag_release:'',  tag_cores:'',  tag_arch:'',  tag_proc:'',  tag_hosts:'',  tag_task:'',  tag_conn:''}
+  d = {tag_vers:'', tag_avMem:'', tag_wireless:'', tag_fw:'', tag_memLoad:'', tag_ip:'', tag_sys:'', tag_wdisk:'', tag_cpu:'', tag_mac:'', tag_rdisk:'', tag_release:'', tag_cores:'', tag_arch:'', tag_proc:'', tag_hosts:'', tag_task:'', tag_conn:''}
   values = getstatus(d)
-  threshold = getvalues(open(paths.THRESHOLD).read(), tag_threshold)
-
+  
   # Logica di controllo del sistema
 
-  connectionCheck(values[tag_conn], threshold[tag_conn])
+  connectionCheck(values[tag_conn], bad_conn)
+  
+  taskCheck(values[tag_proc], bad_proc)
 
-  if int(values[tag_hosts]) > 1:
-    raise Exception, 'Presenza altri host in rete.'
+  if int(values[tag_hosts]) > int(th_host):
+    raise Exception('Presenza altri host in rete.')
   if bool(eval(values[tag_fw])):
-    raise Exception, 'Firewall attivo.'
+    raise Exception('Firewall attivo.')
   if bool(eval(values[tag_wireless])):
-    raise Exception, 'Wireless LAN attiva.'
+    raise Exception('Wireless LAN attiva.')
 
   # Logica di controllo con soglie lette da xml
 
-  if eval(values[tag_avMem]) < eval(threshold[tag_avMem]):
+  if eval(values[tag_avMem]) < th_avMem:
     raise Exception('Memoria non sufficiente.')
-  if eval(values[tag_memLoad]) > eval(threshold[tag_memLoad]):
-    raise Exception, 'Memoria non sufficiente.'
-  if eval(values[tag_cpu]) > eval(threshold[tag_cpu]):
-    raise Exception, 'CPU occupata.'
-  if eval(values[tag_wdisk]) > eval(threshold[tag_wdisk]):
-    raise Exception, 'Eccessiva carico in scrittura del disco.'
-  if eval(values[tag_rdisk]) > eval(threshold[tag_rdisk]):
-    raise Exception, 'Eccessiva carico in lettura del disco.'
+  if eval(values[tag_memLoad]) > th_memLoad:
+    raise Exception('Memoria non sufficiente.')
+  if eval(values[tag_cpu]) > th_cpu:
+    raise Exception('CPU occupata.')
+  if eval(values[tag_wdisk]) > th_wdisk:
+    raise Exception('Eccessiva carico in scrittura del disco.')
+  if eval(values[tag_rdisk]) > th_rdisk:
+    raise Exception('Eccessiva carico in lettura del disco.')
 
   return True
 
 def mediumcheck():
 
-#  d = {tag_avMem, tag_wireless, tag_fw, tag_memLoad, tag_cpu, tag_hosts, tag_task, tag_conn}
-  d = {tag_vers:'', tag_avMem:'', tag_wireless:'',  tag_fw:'',  tag_memLoad:'',  tag_ip:'',  tag_sys:'',  tag_wdisk:'',  tag_cpu:'',  tag_mac:'',  tag_rdisk:'',  tag_release:'',  tag_cores:'',  tag_arch:'',  tag_proc:'',  tag_hosts:'',  tag_task:'',  tag_conn:''}
+  d = {tag_avMem:'', tag_wireless:'',  tag_fw:'',  tag_memLoad:'', tag_cpu:'', tag_hosts:'',  tag_task:'',  tag_conn:''}
   values = getstatus(d)
-  threshold = getvalues(open(paths.THRESHOLD).read(), tag_threshold)
-
+  
   #logica di controllo del sistema
 
-  connectionCheck(values[tag_conn], threshold[tag_conn])
+  connectionCheck(values[tag_conn], bad_conn)
 
-  if values[tag_fw] != False:
-    raise Exception, 'Firewall attivo.'
-  if values[tag_wireless] != False:
-    raise Exception, 'Wireless LAN attiva.'
+  taskCheck(values[tag_proc], bad_proc)
 
-  #logica di controllo con soglie lette da xml
+  if bool(eval(values[tag_fw])):
+    raise Exception('Firewall attivo.')
+  if bool(eval(values[tag_wireless])):
+    raise Exception('Wireless LAN attiva.')
 
-  if values[tag_avMem] > threshold[tag_avMem]:
-    raise Exception, 'Memoria non sufficiente.'
-  if values[tag_memLoad] > threshold[tag_memLoad]:
-    raise Exception, 'Memoria non sufficiente.'
-  if values[tag_cpu] > threshold[tag_cpu]:
-    raise Exception, 'CPU occupata.'
+  # Logica di controllo con soglie lette da xml
 
+  if eval(values[tag_avMem]) < th_avMem:
+    raise Exception('Memoria non sufficiente.')
+  if eval(values[tag_memLoad]) > th_memLoad:
+    raise Exception('Memoria non sufficiente.')
+  if eval(values[tag_cpu]) > th_cpu:
+    raise Exception('CPU occupata.')
+  
   return True
 
 def fastcheck():
@@ -124,32 +137,60 @@ def fastcheck():
   '''
 
 #  d = {tag_memLoad, tag_cpu, tag_task, tag_conn}
-  d = {tag_vers:'', tag_avMem:'', tag_wireless:'',  tag_fw:'',  tag_memLoad:'',  tag_ip:'',  tag_sys:'',  tag_wdisk:'',  tag_cpu:'',  tag_mac:'',  tag_rdisk:'',  tag_release:'',  tag_cores:'',  tag_arch:'',  tag_proc:'',  tag_hosts:'',  tag_task:'',  tag_conn:''}
+  d = {tag_memLoad:'', tag_cpu:'', tag_task:'', tag_conn:''}
   values = getstatus(d)
-  threshold = getvalues(open(paths.THRESHOLD).read(), tag_threshold)
-
+  
   #logica di controllo del sistema
 
-  connectionCheck(values[tag_conn], threshold[tag_conn])
+  connectionCheck(values[tag_conn], bad_conn)
 
-  if values[tag_avMem] > threshold[tag_avMem]:
+  taskCheck(values[tag_proc], bad_proc)
+
+  if eval(values[tag_avMem]) < th_avMem:
     raise Exception('Memoria non sufficiente.')
-    return False
-  if values[tag_memLoad] > threshold[tag_memLoad]:
-    raise Exception, 'Memoria non sufficiente.'
-  if values[tag_cpu] > threshold[tag_cpu]:
-    raise Exception, 'CPU occupata.'
-
+  if eval(values[tag_memLoad]) > th_memLoad:
+    raise Exception('Memoria non sufficiente.')
+  if eval(values[tag_cpu]) > th_cpu:
+    raise Exception('CPU occupata.')
+  
   return True
 
-#creazione dizionario con risposte del SystemProfiler
-def getvalues(string, tag):
 
+def getMac():
+  '''
+  restituisce indirizzo MAC del computer
+  '''
+  d = {tag_mac:''}
+  values = getstatus(d)
+  
+  return values[tag_mac]
+
+
+def getSys():
+  '''
+  restituisce array con informazioni sul sistema utilizzato per il test
+  '''
+  d = {tag_vers:'',  tag_sys:'', tag_mac:'', tag_release:'', tag_cores:'', tag_arch:'', tag_proc:''}
+  values = getstatus(d)
+  
+  r = []
+  
+  for i in values:
+    r.append(values[i])
+  
+  return r
+
+
+def getvalues(string, tag):
+  '''
+  Estrae informazioni dal SystemProfiler 
+  '''
   values = {}
   for subelement in ET.XML(string):
     values.update({subelement.tag:subelement.text})
 
   return values
+
 
 def connectionCheck(connActive, connList):
   '''
@@ -158,14 +199,33 @@ def connectionCheck(connActive, connList):
 
   c = []
   for j in connActive.split(';'):
-    c.append(j.split(':')[1])
-
-  for i in connList.split(';'):
+    c.append(int(j.split(':')[1]))
+  
+  for i in connList:
     if i in c:
       raise Exception, 'Sono attive connessioni non desiderate.'
 
   return True
 
-if __name__ == '__main__':
-  print 'Test sysmonitor: %s' % checkall()
+def taskCheck(taskActive, taskList):
+  '''
+  Ettettua il controllo sui processi
+  '''
 
+  t = []
+  for j in taskActive.split(';'):
+    t.append(str(j))
+
+  for i in taskList:
+    if i in t:
+      raise Exception, 'Sono attivi processi non desiderati.'
+
+  return True
+
+if __name__ == '__main__':
+  print '_______________________________________\n'
+  print 'Test sysmonitor fastcheck: %s' % fastcheck()
+  print 'Test sysmonitor mediumcheck: %s' % mediumcheck()
+  print 'Test sysmonitor checkall: %s' % checkall()
+  print '\nTest sysmonitor getMac: %s' % getMac()
+  print '\nTest sysmonitor getSys: %s' % getSys()
