014780fb680364b0d05a79385c2473d8ed3a18d8
gpantanetti <gpantanetti@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2010-10-22 10:26:59 +0000

    Modificato sistema di logging in TimedRotatingFileHandler.

--- trunk/nemesys/logger.py
+++ trunk/nemesys/logger.py
@@ -17,25 +17,27 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from os import path
+from os import mkdir
 import logging.config
 import paths
 import sys
 
 configfile = paths.CONF_LOG
+logfile = paths.FILE_LOG
 
 default = '''
 [loggers]
 keys=root
 
 [handlers]
-keys=console
+keys=console,rotating
 
 [formatters]
 keys=formatter
 
 [logger_root]
 level=INFO
-handlers=console
+handlers=console,rotating
 
 [handler_console]
 class=StreamHandler
@@ -43,80 +45,22 @@ level=INFO
 formatter=formatter
 args=(sys.stdout,)
 
-[formatter_formatter] 
-format=%(asctime)s NeMeSys %(filename)s.%(funcName)s():%(lineno)d [%(levelname)s] %(message)s
-datefmt=%b %d %H:%M:%S
-'''
-
-default_win = '''
-[loggers]
-keys=root
-
-[handlers]
-keys=console,ntevent
-
-[formatters]
-keys=formatter
-
-[logger_root]
-level=INFO
-handlers=console,ntevent
-
-[handler_console]
-class=StreamHandler
+[handler_rotating]
+class=handlers.TimedRotatingFileHandler
 level=INFO
 formatter=formatter
-args=(sys.stdout,)
-
-[handler_ntevent]
-class=handlers.NTEventLogHandler
-level=ERROR
-formatter=formatter
-args=('NeMeSys', '', 'Application')
+args=("''' + logfile + '''", 'midnight', 1, 5)
 
 [formatter_formatter] 
 format=%(asctime)s NeMeSys %(filename)s.%(funcName)s():%(lineno)d [%(levelname)s] %(message)s
 datefmt=%b %d %H:%M:%S
 '''
 
-default_posix = '''
-[loggers]
-keys=root
-
-[handlers]
-keys=console,syslog
-
-[formatters]
-keys=formatter
-
-[logger_root]
-level=INFO
-handlers=console,syslog
-
-[handler_console]
-class=StreamHandler
-level=INFO
-formatter=formatter
-args=(sys.stdout,)
-
-[handler_syslog]
-class=handlers.SysLogHandler
-level=ERROR
-formatter=formatter
-args=('/dev/log', handlers.SysLogHandler.LOG_USER)
-
-[formatter_formatter] 
-format=%(asctime)s NeMeSys %(filename)s.%(funcName)s():%(lineno)d [%(levelname)s] %(message)s
-datefmt=%b %d %H:%M:%S
-'''
+if not path.exists(paths.LOG_DIR):
+  mkdir(paths.LOG_DIR)
 
 # Se il file configurazione di log non esiste, creane uno con le impostazioni base
 if (not path.exists(configfile)):
-
-  if sys.platform[0:3] == 'win':
-    default = default_win
-  elif sys.platform[0:6] == 'darwin' or sys.platform[0:5] == 'linux':
-    default = default_posix
 
   with open(configfile, 'w') as file:
     s = str(default)
