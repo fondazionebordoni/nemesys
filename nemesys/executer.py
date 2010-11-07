fe1fd3c67f01f0c17d9aa2155359821296cd86f7
Giuseppe Pantanetti <gpantanetti@users.noreply.github.com>
2010-11-07 15:35:18 +0000

    Sesta release (beta) del software utente. Developer release 8.

--- nemesys/executer.py
+++ nemesys/executer.py
@@ -53,7 +53,7 @@ status_sem = Semaphore()
 logger = logging.getLogger()
 errors = Errorcoder(paths.CONF_ERRORS)
 current_status = status.LOGO
-VERSION = '1.6.5.9'
+VERSION = '1.6.5.8'
 
 # Numero massimo di misure per ora
 MAX_MEASURES_PER_HOUR = 1
