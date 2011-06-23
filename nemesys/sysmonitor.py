b797b7aa212bc55e2bebb3b650fe47a72cdcb68f
gpantanetti <gpantanetti@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2011-06-23 11:54:18 +0000

    Undebug.

--- nemesys/sysmonitor.py
+++ nemesys/sysmonitor.py
@@ -256,7 +256,6 @@ def checkhosts(up, down, ispid):
   
   if (mask != 0):  
     value = checkhost.countHosts(ip, mask, up, down, ispid, th_host)
-    value = 1
     logger.info('Trovati %d host in rete.' % value)
       
     if value <= 0:
