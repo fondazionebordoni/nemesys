528d37751e80841013f2ad8279a58d0002282f1b
albenzio.cirillo <albenzio.cirillo@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2011-08-02 15:41:30 +0000

    Creazione della classe SysmonitorException in sysmonitorexception.py

--- branches/contabit/nemesys/sysmonitorexception.py
+++ branches/contabit/nemesys/sysmonitorexception.py
@@ -1,5 +1,18 @@
 
 
 class SysmonitorException(Exception):
-  
-  
+
+    def __init__(self, alert_type, message):
+        if isinstance (color, Status):
+            self._alert_type = alert_type
+        else:
+            self._alert_type = alert_type.decode('utf-8')
+        
+        self._message = message.decode('utf-8')
+ 
+ #Error at sysmonitor 81
+ FAILPROF = SysmonitorException('FAILPROF','Non sono riuscito a trovare lo stato del computer con SystemProfiler.')      
+ #Error at sysmonitor 94 110 123 170 201
+ FAILREADPARAM = SysmonitorException('FAILREADPARAM','Errore in SystemProfiler.')
+ #Error at sysmonitor 128
+ FAILVALUEPARAM = SysmonitorException('FAILVALUEPARAM','Errore in SystemProfiler.')       
