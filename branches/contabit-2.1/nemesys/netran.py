1f36f49b11a93f21a1cc52bc4ac81dcc3dab5ec6
domenico.izzo <domenico.izzo@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2011-09-13 10:18:59 +0000

    Sistemate tabulazioni in netran.py

--- branches/contabit-2.1/nemesys/netran.py
+++ branches/contabit-2.1/nemesys/netran.py
@@ -43,248 +43,248 @@ condition = Condition()
 
 class Device:
 
-    def __init__(self): None
-
-    def getdev(self,req=None):
-        device=sniffer.getdev(req)
-        return device
+  def __init__(self): None
+  
+  def getdev(self,req=None):
+    device=sniffer.getdev(req)
+    return device
 
 
 class Sniffer(Thread):
 
-    def __init__(self,dev,buff=32*1024000,snaplen=8192,timeout=1,promisc=1,debug=0):
-        Thread.__init__(self)
-        global debug_mode
-        global sniffer_init
-        global run_sniffer
-        debug_mode=sniffer.debugmode(debug)
-        sniffer_init=sniffer.initialize(dev,buff,snaplen,timeout,promisc)
-        if (sniffer_init['err_flag']==0):
-            run_sniffer=1
-
-    def run(self,sniffer_mode=0):
-        global run_sniffer
-        global sniffer_flag
-        sniffer_flag=1
-        while (run_sniffer==1):
-            global analyzer_flag
-            global shared_buffer
-            global condition
-            if (analyzer_flag==1):
-                condition.acquire()
-                if (len(shared_buffer) == 100):
-                    condition.wait()
-                loop=0
-                while (loop == 0 and analyzer_flag == 1):
-                    sniffer_data=sniffer.start(sniffer_mode)
-                    loop=sniffer_data['blocks_num']
-                shared_buffer.append(sniffer_data)
-                condition.notify()
-                condition.release()
-            else:
-                black_hole=sniffer.start(sniffer_mode)
-        sniffer_flag=0
-
-
-    def stop(self):
-        global run_sniffer
-        global shared_buffer
-        run_sniffer=0
-        while (sniffer_flag != 0): None
-        sniffer_stop=sniffer.stop()
-        return sniffer_stop
-
-
-    def getstat(self):
-        sniffer_stat=sniffer.getstat()
-        return sniffer_stat
-
-
-    def join(self, timeout=None):
-        Thread.join(self, timeout)
+  def __init__(self,dev,buff=32*1024000,snaplen=8192,timeout=1,promisc=1,debug=0):
+    Thread.__init__(self)
+    global debug_mode
+    global sniffer_init
+    global run_sniffer
+    debug_mode=sniffer.debugmode(debug)
+    sniffer_init=sniffer.initialize(dev,buff,snaplen,timeout,promisc)
+    if (sniffer_init['err_flag']==0):
+      run_sniffer=1
+  
+  def run(self,sniffer_mode=0):
+    global run_sniffer
+    global sniffer_flag
+    sniffer_flag=1
+    while (run_sniffer==1):
+      global analyzer_flag
+      global shared_buffer
+      global condition
+      if (analyzer_flag==1):
+        condition.acquire()
+        if (len(shared_buffer) == 100):
+          condition.wait()
+        loop=0
+        while (loop == 0 and analyzer_flag == 1):
+          sniffer_data=sniffer.start(sniffer_mode)
+          loop=sniffer_data['blocks_num']
+        shared_buffer.append(sniffer_data)
+        condition.notify()
+        condition.release()
+      else:
+        black_hole=sniffer.start(sniffer_mode)
+    sniffer_flag=0
+  
+  
+  def stop(self):
+    global run_sniffer
+    global shared_buffer
+    run_sniffer=0
+    while (sniffer_flag != 0): None
+    sniffer_stop=sniffer.stop()
+    return sniffer_stop
+  
+  
+  def getstat(self):
+    sniffer_stat=sniffer.getstat()
+    return sniffer_stat
+  
+  
+  def join(self, timeout=None):
+    Thread.join(self, timeout)
 
 
 class Contabyte(Thread):
 
-    def __init__(self,dev,nem,debug=0):
-        Thread.__init__(self)
-        global debug_mode
-        global contabyte_init
-        global run_contabyte
-        debug_mode=contabyte.debugmode(debug)
-        contabyte_init=contabyte.initialize(dev,nem)
-        if (contabyte_init==0):
-            run_contabyte=1
+  def __init__(self,dev,nem,debug=0):
+    Thread.__init__(self)
+    global debug_mode
+    global contabyte_init
+    global run_contabyte
+    debug_mode=contabyte.debugmode(debug)
+    contabyte_init=contabyte.initialize(dev,nem)
+    if (contabyte_init==0):
+      run_contabyte=1
 
 
-    def run(self):
-        global run_contabyte
-        global analyzer_flag
-        analyzer_flag=1
-        while (run_contabyte==1):
-            global sniffer_flag
-            global shared_buffer
-            global condition
-            if (sniffer_flag==1):
-                condition.acquire()
-                if (len(shared_buffer) == 0):
-                    if (analyzer_flag==1):
-                        condition.wait()
-                else:
-                    contabyte_data=shared_buffer.popleft()
-                    contabyte.analyze(contabyte_data['py_byte_array'],contabyte_data['block_size'],contabyte_data['blocks_num'],contabyte_data['datalink'])
-                    condition.notify()
-                    condition.release()
-            else:
-                run_contabyte=0
-        analyzer_flag=0
-
-
-    def stop(self):
-        global analyzer_flag
-        global shared_buffer
-        global run_contabyte
-        analyzer_flag=2
-        while (len(shared_buffer) != 0): None
+  def run(self):
+    global run_contabyte
+    global analyzer_flag
+    analyzer_flag=1
+    while (run_contabyte==1):
+      global sniffer_flag
+      global shared_buffer
+      global condition
+      if (sniffer_flag==1):
+        condition.acquire()
+        if (len(shared_buffer) == 0):
+          if (analyzer_flag==1):
+            condition.wait()
+        else:
+          contabyte_data=shared_buffer.popleft()
+          contabyte.analyze(contabyte_data['py_byte_array'],contabyte_data['block_size'],contabyte_data['blocks_num'],contabyte_data['datalink'])
+          condition.notify()
+          condition.release()
+      else:
         run_contabyte=0
-        contabyte_stop=contabyte.close()
-        return contabyte_stop
+    analyzer_flag=0
 
 
-    def getstat(self):
-        contabyte_stat=contabyte.getstat()
-        return contabyte_stat
+  def stop(self):
+    global analyzer_flag
+    global shared_buffer
+    global run_contabyte
+    analyzer_flag=2
+    while (len(shared_buffer) != 0): None
+    run_contabyte=0
+    contabyte_stop=contabyte.close()
+    return contabyte_stop
 
 
-    def join(self, timeout=None):
-        Thread.join(self, timeout)
+  def getstat(self):
+    contabyte_stat=contabyte.getstat()
+    return contabyte_stat
+
+
+  def join(self, timeout=None):
+    Thread.join(self, timeout)
 
 
 
 
 if __name__ == '__main__':
 
-    mydev='192.168.208.53'
-    mynem='194.244.5.206'
-    debug=1
+  mydev='192.168.208.53'
+  mynem='194.244.5.206'
+  debug=1
 
-    print "\nDevices:"
+  print "\nDevices:"
 
-    mydevice=Device()
+  mydevice=Device()
 
-    print "\nFirst Request: All Devices"
+  print "\nFirst Request: All Devices"
 
-    device=mydevice.getdev()
+  device=mydevice.getdev()
 
-    if (device!=None):
-        print
-        keys=device.keys()
-        keys.sort()
-        for key in keys:
-            print "%s \t %s" % (key,device[key])
-    else:
-        print "No Devices"
-
-    print "\nSecond Request: Device by IP not assigned to the machine"
-
-    device=mydevice.getdev('194.244.5.206')
-
-    if (device!=None):
-        print
-        keys=device.keys()
-        keys.sort()
-        for key in keys:
-            print "%s \t %s" % (key,device[key])
-    else:
-        print "No Devices"
-
-    print "\nThird Request: Device by IP assigned to the machine"
-
-    device=mydevice.getdev(mydev)
-
-    if (device!=None):
-        print
-        keys=device.keys()
-        keys.sort()
-        for key in keys:
-            print "%s \t %s" % (key,device[key])
-    else:
-        print "No Devices"
-
-
-    print "\nInitialize Sniffer And Contabyte...."
-
-    mysniffer=Sniffer(mydev,32*1024000,150,1,1,debug)
-
-    print "Debug Mode Sniffer:", debug_mode
-    if (sniffer_init['err_flag']==0):
-        print "Success Sniffer\n"
-    else:
-        print "Fail Sniffer:",sniffer_init['err_flag']
-        print "Error Sniffer:",sniffer_init['err_str']
-
-    mycontabyte=Contabyte(mydev,mynem,debug)
-
-    print "Debug Mode Contabyte:", debug_mode
-    if (contabyte_init==0):
-        print "Success Contabyte\n"
-    else:
-        print "Fail Contabyte\n"
-
-
-    print "Start Sniffer And Contabyte...."
-
-    mysniffer.start()
-    mycontabyte.start()
-
-    print "Sniffing And Analyzing...."
-
-    raw_input("Enter When Finished!!")
-    #time.sleep(30)
-
-    contabyte_stop=mycontabyte.stop()
-    if (contabyte_stop==0):
-        print "Success Contabyte"
-    else:
-        print "Fail\n"
-
-    sniffer_stop=mysniffer.stop()
-    if (sniffer_stop['err_flag']==0):
-        print "Success Sniffer"
-    else:
-        print "Fail:",sniffer_stop['err_flag']
-        print "Error:",sniffer_stop['err_str']
-
-    print "Sniffer And Contabyte Statistics:\n"
-
-    contabyte_stat=mycontabyte.getstat()
-    if (contabyte_stat!=None):
-        keys=contabyte_stat.keys()
-        keys.sort()
-        for key in keys:
-            print "Key: %s \t Value: %s" % (key,contabyte_stat[key])
-    else:
-        print "No Statistics"
-
+  if (device!=None):
     print
+    keys=device.keys()
+    keys.sort()
+    for key in keys:
+      print "%s \t %s" % (key,device[key])
+  else:
+    print "No Devices"
 
-    sniffer_stat=mysniffer.getstat()
-    if (sniffer_stat!=None):
-        keys=sniffer_stat.keys()
-        keys.sort()
-        for key in keys:
-            print "Key: %s \t Value: %s" % (key,sniffer_stat[key])
-    else:
-        print "No Statistics"
+  print "\nSecond Request: Device by IP not assigned to the machine"
 
-    print "\nSniffer And Contabyte Join...."
+  device=mydevice.getdev('194.244.5.206')
 
-    #print("Sniffer Flag:"+str(sniffer_flag)+" Analyzer Flag:"+str(analyzer_flag))
+  if (device!=None):
+    print
+    keys=device.keys()
+    keys.sort()
+    for key in keys:
+      print "%s \t %s" % (key,device[key])
+  else:
+    print "No Devices"
 
-    mycontabyte.join()
+  print "\nThird Request: Device by IP assigned to the machine"
+
+  device=mydevice.getdev(mydev)
+
+  if (device!=None):
+    print
+    keys=device.keys()
+    keys.sort()
+    for key in keys:
+      print "%s \t %s" % (key,device[key])
+  else:
+    print "No Devices"
+
+
+  print "\nInitialize Sniffer And Contabyte...."
+
+  mysniffer=Sniffer(mydev,32*1024000,150,1,1,debug)
+
+  print "Debug Mode Sniffer:", debug_mode
+  if (sniffer_init['err_flag']==0):
+    print "Success Sniffer\n"
+  else:
+    print "Fail Sniffer:",sniffer_init['err_flag']
+    print "Error Sniffer:",sniffer_init['err_str']
+
+  mycontabyte=Contabyte(mydev,mynem,debug)
+
+  print "Debug Mode Contabyte:", debug_mode
+  if (contabyte_init==0):
+    print "Success Contabyte\n"
+  else:
+    print "Fail Contabyte\n"
+
+
+  print "Start Sniffer And Contabyte...."
+
+  mysniffer.start()
+  mycontabyte.start()
+
+  print "Sniffing And Analyzing...."
+
+  raw_input("Enter When Finished!!")
+  #time.sleep(30)
+
+  contabyte_stop=mycontabyte.stop()
+  if (contabyte_stop==0):
     print "Success Contabyte"
-    mysniffer.join()
+  else:
+    print "Fail\n"
+
+  sniffer_stop=mysniffer.stop()
+  if (sniffer_stop['err_flag']==0):
     print "Success Sniffer"
+  else:
+    print "Fail:",sniffer_stop['err_flag']
+    print "Error:",sniffer_stop['err_str']
+
+  print "Sniffer And Contabyte Statistics:\n"
+
+  contabyte_stat=mycontabyte.getstat()
+  if (contabyte_stat!=None):
+    keys=contabyte_stat.keys()
+    keys.sort()
+    for key in keys:
+      print "Key: %s \t Value: %s" % (key,contabyte_stat[key])
+  else:
+    print "No Statistics"
+
+  print
+
+  sniffer_stat=mysniffer.getstat()
+  if (sniffer_stat!=None):
+    keys=sniffer_stat.keys()
+    keys.sort()
+    for key in keys:
+      print "Key: %s \t Value: %s" % (key,sniffer_stat[key])
+  else:
+    print "No Statistics"
+
+  print "\nSniffer And Contabyte Join...."
+
+  #print("Sniffer Flag:"+str(sniffer_flag)+" Analyzer Flag:"+str(analyzer_flag))
+
+  mycontabyte.join()
+  print "Success Contabyte"
+  mysniffer.join()
+  print "Success Sniffer"
 
 
 
