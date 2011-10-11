b62c09b156afdfc2a503cd08b180d2a0d33b7d31
gpantanetti <gpantanetti@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2011-10-11 17:47:33 +0000

    Trivial commit

--- trunk/nemesys/proof.py
+++ trunk/nemesys/proof.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# TODO Estendere Proof per la memorizzazione dei dati di contabit
-
 from datetime import datetime
 from statistics import Statistics
 
