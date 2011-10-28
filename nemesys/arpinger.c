fbeae133436b8b68c1d38dd591542ed793c02ca0
domenico.izzo <domenico.izzo@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2011-10-28 17:30:01 +0000

    Migliorato la funzione find_device in arpinger.c e sniffer.c per la selezione dei device di rete nei sistemi windows

--- nemesys/arpinger.c
+++ nemesys/arpinger.c
@@ -69,6 +69,7 @@ void find_devices()
     WSADATA wsa_Data;
     char HostName[255];
     struct hostent *host_entry;
+	int addr_num = 0;
     #endif
 
     ind_dev=0;
@@ -112,6 +113,8 @@ void find_devices()
             addr.s_addr = ((struct sockaddr_in *)(dl->addresses->addr))->sin_addr.s_addr;
             ip = inet_ntoa(addr);
 
+			//printf("\nProvo: %s",ip);
+
             IpInNet = ip_in_net(ip,device[ind_dev].net,device[ind_dev].mask);
 
             if(IpInNet != 1)
@@ -120,7 +123,14 @@ void find_devices()
                 WSAStartup(0x101,&wsa_Data);
                 gethostname(HostName, 255);
                 host_entry = gethostbyname(HostName);
-                ip = inet_ntoa (*(struct in_addr *)*host_entry->h_addr_list);
+				addr_num = 0;
+				while((IpInNet != 1) && (host_entry->h_addr_list[addr_num] != NULL))
+                {
+					ip = inet_ntoa (*(struct in_addr *)(host_entry->h_addr_list)[addr_num]);
+					//printf("\nProvo: %s",ip);
+					IpInNet = ip_in_net(ip,device[ind_dev].net,device[ind_dev].mask);
+					addr_num++;
+				}
                 WSACleanup();
                 #else
                 while((IpInNet != 1) && (dl->addresses->next))
@@ -128,13 +138,22 @@ void find_devices()
                     dl->addresses=dl->addresses->next;
                     addr.s_addr = ((struct sockaddr_in *)(dl->addresses->addr))->sin_addr.s_addr;
                     ip = inet_ntoa(addr);
+					//printf("\nProvo: %s",ip);
                     IpInNet = ip_in_net(ip,device[ind_dev].net,device[ind_dev].mask);
                 }
                 #endif
             }
 
-            device[ind_dev].ip=PyMem_New(char,strlen(ip)+1);
-            memcpy(device[ind_dev].ip,ip,strlen(ip)+1);
+			if(IpInNet == 1)
+            {
+				device[ind_dev].ip=PyMem_New(char,strlen(ip)+1);
+				memcpy(device[ind_dev].ip,ip,strlen(ip)+1);
+			}
+			else
+			{
+				device[ind_dev].ip="0.0.0.0";
+			}
+
 
         }
         else
@@ -166,6 +185,8 @@ void initialize(u_char *dev, int promisc, int timeout, int snaplen, int buffer)
             num_dev=i;
         }
     }
+
+	//printf("Device Scelto: %i",num_dev);
 
     if (num_dev==0)
     {sprintf(err_str,"Device Not Found or Not Initialized");err_flag=-1;return;}
