9abbe1bc6afc3ea156ee7bdca80ac54bfa7fab21
Giuseppe Pantanetti <gpantanetti@users.noreply.github.com>
2012-01-12 17:42:47 +0000

    Seconda major release del software utente. Versione per compatibilità Python 2.6

--- nemesys/arpinger_compile.py
+++ nemesys/arpinger_compile.py
@@ -4,9 +4,8 @@ module = Extension('arpinger',
                     define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '0')],
                     include_dirs = ['.'],
-                    libraries = ['python2.7','pcap'],
-                    #library_dirs = ['/usr/include/python2.7'],
-                    library_dirs = ['/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7'],
+                    libraries = ['python2.6','pcap'],
+                    library_dirs = ['/System/Library/Frameworks/Python.framework/Versions/2.6/include/python2.6'],
                     sources = ['arpinger.c'])
 
 setup (name = 'nemesys',
