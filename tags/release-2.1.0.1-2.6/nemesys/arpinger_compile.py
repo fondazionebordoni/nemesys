c0feb34dd78b70de217b970b549236e8d46c0d5f
gpantanetti <gpantanetti@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2012-01-12 17:36:49 +0000

    Seconda major release del software utente. Versione per compatibilità Python 2.6

---
+++ tags/release-2.1.0.1-2.6/nemesys/arpinger_compile.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+
+module = Extension('arpinger',
+                    define_macros = [('MAJOR_VERSION', '1'),
+                                     ('MINOR_VERSION', '0')],
+                    include_dirs = ['.'],
+                    libraries = ['python2.7','pcap'],
+                    #library_dirs = ['/usr/include/python2.7'],
+                    library_dirs = ['/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7'],
+                    sources = ['arpinger.c'])
+
+setup (name = 'nemesys',
+       version = '2.1',
+       description = 'nemesys qos',
+       author = 'Domenico Izzo',
+       author_email = 'dizzo@fub.it',
+       url = 'nemesys-qos.googlecode.com',
+       long_description = '''
+NONE
+''',
+       ext_modules = [module])
