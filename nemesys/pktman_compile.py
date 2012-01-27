9581ac2b486df9ec27d449ca1216fe39790964a5
domenico.izzo <domenico.izzo@afc7da65-08b4-2af4-0fdd-4e424827a58f>
2012-01-27 13:51:03 +0000

    New module pktman.c (Packet Manager) to replace arpinger.c and sniffer.c

--- nemesys/pktman_compile.py
+++ nemesys/pktman_compile.py
@@ -3,23 +3,23 @@ from distutils.core import setup, Extension
 import platform
 
 if (platform.python_version_tuple()[1] == 7):
-  module = Extension('sniffer',
+  module = Extension('pktman',
     define_macros = [('MAJOR_VERSION', '1'),
                      ('MINOR_VERSION', '0')],
     include_dirs = ['.'],
     libraries = ['python2.7', 'pcap'],
     #library_dirs = ['/usr/include/python2.7'],
     library_dirs = ['/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7'],
-    sources = ['sniffer.c'])
+    sources = ['pktman.c'])
 else:
-  module = Extension('sniffer',
+  module = Extension('pktman',
     define_macros = [('MAJOR_VERSION', '1'),
                      ('MINOR_VERSION', '0')],
     include_dirs = ['.'],
     libraries = ['python2.6', 'pcap'],
     #library_dirs = ['/usr/include/python2.6'],
     library_dirs = ['/System/Library/Frameworks/Python.framework/Versions/2.6/include/python2.6'],
-    sources = ['sniffer.c'])
+    sources = ['pktman.c'])
 
 setup (name = 'nemesys',
    version = '2.1',
