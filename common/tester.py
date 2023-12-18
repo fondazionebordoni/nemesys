db3e80f2763ed205f9ee00feb07e15174f0f0a32
Giuseppe Pantanetti <gpantanetti@fub.it>
2023-12-18 17:34:01 +0100

    Added parameters for bandwidth up to 5 Gbps

--- common/tester.py
+++ common/tester.py
@@ -32,9 +32,14 @@ from common.testerhttpdown import HttpTesterDown
 from common.testerhttpup import HttpTesterUp
 
 HTTP_BUFF = 8 * 1024
-BW_3M = 3000000
-BW_100M = 100000000
-BW_500M = 500000000
+BW_3M = 3 * 10**6
+BW_100M = 100 * 10**6
+BW_500M = 500 * 10**6
+BW_1000M = 1 * 10**9
+BW_2000M = 2 * 10**9
+BW_2500M = 2.5 * 10**9
+BW_5000M = 5 * 10**9
+
 logger = logging.getLogger(__name__)
 
 
@@ -48,12 +53,32 @@ class Tester(object):
 
     def testhttpdown(self, callback_update_speed=None, bw=BW_100M):
         url = "http://%s/file.rnd" % self._host.ip
-        if bw >= BW_500M and utils.is_darwin():
+        if bw <= BW_500M and utils.is_darwin():
             num_sessions = 1
             buffer_size = 2 * 8192
-        else:
+        elif bw <= BW_2500M:
             num_sessions = 7
             buffer_size = 8192
+        elif bw <= BW_5000M:
+            num_sessions = 10
+            buffer_size = 3 * 8192
+            if utils.is_windows():
+                num_sessions = 12
+                buffer_size = 3 * 8192
+            if utils.is_darwin():
+                num_sessions = 8
+                buffer_size = 3 * 8192
+        else:
+            num_sessions = 14
+            buffer_size = 8 * 8192
+            if utils.is_windows():
+                num_sessions = 11
+                buffer_size = 11 * 8192
+            if utils.is_darwin():
+                num_sessions = 16
+                buffer_size = 8 * 8192
+
+        logger.debug(f"Variabili di misura num_session={num_sessions}, buffer_size={buffer_size}")
         return self._testerhttpdown.test(url, callback_update_speed, num_sessions=num_sessions, buffer_size=buffer_size)
 
     def testhttpup(self, callback_update_speed=None, bw=BW_100M):
@@ -68,16 +93,49 @@ class Tester(object):
         elif bw <= BW_100M:
             num_sessions = 6
             tcp_window_size = 65 * 1024
-        else:
-            num_sessions = 1
-            if utils.is_windows():
-                tcp_window_size = 256 * 1024
-            else:
+        elif bw <= BW_1000M:
+            num_sessions = 7
+            tcp_window_size = -1
+            buffer_size = 8192
+            if utils.is_darwin():
+                num_sessions = 4
+                buffer_size = 3 * 1024
                 tcp_window_size = -1
-                if utils.is_darwin():
-                    buffer_size = 3 * 8129
-        return self._testerhttpup.test(url, callback_update_speed, num_sessions=num_sessions,
-                                       tcp_window_size=tcp_window_size, buffer_size=buffer_size)
+            if utils.is_windows():
+                num_sessions = 7
+                buffer_size = 8192
+                tcp_window_size = 1024 * 1024
+        elif bw <= BW_2000M:
+            num_sessions = 9
+            buffer_size = 8192
+            tcp_window_size = -1
+            if utils.is_windows():
+                num_sessions = 5
+                buffer_size = 4 * 8192
+                tcp_window_size = 1024 * 1024
+            if utils.is_darwin():
+                num_sessions = 3
+                tcp_window_size = -1
+                buffer_size = 5 * 1024
+
+        else:
+            num_sessions = 3
+            tcp_window_size = -1
+            if utils.is_windows():
+                buffer_size = 8 * 1024
+                num_sessions = 3
+                tcp_window_size = 256 * 1024
+            if utils.is_darwin():
+                num_sessions = 3
+                tcp_window_size = -1
+                buffer_size = 5 * 1024
+
+        logger.debug(
+            f"Variabili di misura num_session={num_sessions}, tcp_window_size={tcp_window_size}, buffer_size={buffer_size}"
+        )
+        return self._testerhttpup.test(
+            url, callback_update_speed, num_sessions=num_sessions, tcp_window_size=tcp_window_size, buffer_size=buffer_size
+        )
 
     def testping(self, timeout=10):
         # si utilizza funzione ping.py
@@ -93,80 +151,91 @@ class Tester(object):
         if rtt is None:
             raise MeasurementException("Ping timeout", nem_exceptions.PING_TIMEOUT)
 
-        return Proof(test_type='ping', start_time=start, duration=rtt * 1000, bytes_nem=0)
+        return Proof(test_type="ping", start_time=start, duration=rtt * 1000, bytes_nem=0)
 
 
 def main():
     import time
+
     # Aggancio opzioni da linea di comando
 
-    parser = OptionParser(version="0.10.1.$Rev$",
-                          description="A simple bandwidth tester able to perform HTTP upload/download and PING tests.")
-    parser.add_option("-t", "--type", choices=('down', 'up', 'ping'),
-                      dest="testtype", default="down", type="choice",
-                      help="Choose the type of test to perform: down (default), up, ping")
-    parser.add_option("-b", "--bandwidth", dest="bandwidth", default="100M", type="string",
-                      help="The expected bandwith to measure, used in upload tests, e.g. 512k, 2M")
-    parser.add_option("-n", "--num-tests", dest="num_tests", default="1", type="int",
-                      help="Number of tests to perform")
-    parser.add_option("-H", "--host", dest="host", default="193.104.137.133",
-                      help="An ipaddress or FQDN of server host")
+    parser = OptionParser(
+        version="0.10.1.$Rev$", description="A simple bandwidth tester able to perform HTTP upload/download and PING tests."
+    )
+    parser.add_option(
+        "-t",
+        "--type",
+        choices=("down", "up", "ping"),
+        dest="testtype",
+        default="down",
+        type="choice",
+        help="Choose the type of test to perform: down (default), up, ping",
+    )
+    parser.add_option(
+        "-b",
+        "--bandwidth",
+        dest="bandwidth",
+        default="100M",
+        type="string",
+        help="The expected bandwith to measure, used in upload tests, e.g. 512k, 2M",
+    )
+    parser.add_option("-n", "--num-tests", dest="num_tests", default="1", type="int", help="Number of tests to perform")
+    parser.add_option("-H", "--host", dest="host", default="193.104.137.133", help="An ipaddress or FQDN of server host")
 
     (options, _) = parser.parse_args()
     try:
-        dev = iptools.get_dev()
+        dev = iptools.get_dev(host=options.host, port=80)
     except Exception:
-        try:
-            dev = iptools.get_dev(host=options.host, port=80)
-        except Exception:
-            print("Impossibile ottenere indirizzo e device, verificare la connessione all'host")
-            import sys
-            sys.exit(2)
+        print("Impossibile ottenere indirizzo e device, verificare la connessione all'host")
+        import sys
+
+        sys.exit(2)
+    print(f"Misure su interfaccia: {dev}")
     t = Tester(dev, Host(options.host), timeout=10)
     if options.bandwidth.endswith("M"):
         bw = int(options.bandwidth[:-1]) * 1000000
     elif options.bandwidth.endswith("k"):
         bw = int(options.bandwidth[:-1]) * 1000
     else:
-        print("Please specify bandwith in the form of 2M or 512k")
+        print("Please specify bandwidth in the form of 2M or 512k")
         return
 
     print("===============================================")
-    print(('Testing: %s' % options.host))
+    print(f"Testing: {options.host}")
     for i in range(1, options.num_tests + 1):
         print("-----------------------------------------------")
         if i != 1:
-            print("Sleeping....")
+            print("Sleeping...")
             print("-----------------------------------------------")
             time.sleep(10)
-        print(('test %d %s' % (i, options.testtype)))
-        if options.testtype == 'up':
+        print(f"test {i} {options.testtype}")
+        if options.testtype == "up":
             try:
                 res = t.testhttpup(None, bw=bw)
                 printout_http(res)
             except MeasurementException as e:
-                print(("Error: [%d] %s" % (e.errorcode, str(e))))
-        elif options.testtype == 'ping':
+                print("Error: [%d] %s" % (e.errorcode, str(e)))
+        elif options.testtype == "ping":
             try:
                 res = t.testping()
-                print(("Ping: %.2f milliseconds" % res.duration))
+                print("Ping: %.2f milliseconds" % res.duration)
             except Exception as e:
-                print(("Error: [%d] %s" % (e.errorcode, str(e))))
+                print("Error: [%d] %s" % (e.errorcode, str(e)))
         else:
             try:
                 res = t.testhttpdown(bw=bw)
                 printout_http(res)
             except MeasurementException as e:
-                print(("Error: %s" % str(e)))
+                print("Error: %s" % str(e))
     print("===============================================")
 
 
 def printout_http(res):
-    print(("Medium speed: %d kbps" % (int(res.bytes_tot * 8 / float(res.duration)))))
-    print(("Spurious traffic: %.2f%%" % (res.spurious * 100.0)))
+    print("Medium speed: %d kbps" % (int(res.bytes_tot * 8 / float(res.duration))))
+    print("Spurious traffic: %.2f%%" % (res.spurious * 100.0))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from nemesys import log_conf
 
     log_conf.init_log()
