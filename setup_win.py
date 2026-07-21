45c189748d8cecb8d0f342b4f250cd7c709e8589
Giordano Mecarelli <gmecarelli@fub.it>
2026-07-21 15:56:06 +0200

    Adeguata sintassi alla versione recente di py2exe

--- setup_win.py
+++ setup_win.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# setup for py2exe on Windows
+# freeze script for py2exe on Windows (API py2exe>=0.14, no piu' distutils)
 #
 import os
-import py2exe  # noqa
 import re
 import sys
-from distutils.core import setup
+
+from py2exe import freeze
 
 sys.path.append(os.path.join('.', 'nemesys'))
 sys.path.append(os.path.join('.', 'common'))
@@ -43,41 +43,29 @@ def sub_iss_file(f, ver):
         fd.write(filedata)
 
 
-class Target:
-    def __init__(self, **kw):
-        self.__dict__.update(kw)
-        # for the versioninfo resources
-        self.version = get_version()
-        self.company_name = 'Fondazione Ugo Bordoni'
-        self.copyright = '(c)2010-2017 Fondazione Ugo Bordoni'
-        # self.name = "MisuraInternet Speed Test"
-
-nemesys_service = Target(
-    # used for the versioninfo resource
-    description='Nemesys Service',
-    # what to build.  For a service, the module name (not the
-    # filename) must be specified!
-    modules=['nemesys.Nemesys'],
-    icon_resources=[(1, '.\\icons\\nemesys.ico')],
-    cmdline_style='pywin32',
-    name='Nemesys'
-    )
-
-
-setup(
-    options={
-        'py2exe': {
-            'packages': ['encodings', 'common', 'nemesys', 'charset_normalizer'],
-            'includes': ['_cffi_backend'],
-            'optimize': 2,
-        }
+freeze(
+    service=[{
+        # used for the versioninfo resource
+        'description': 'Nemesys Service',
+        # what to build.  For a service, the module name (not the
+        # filename) must be specified!
+        'modules': ['nemesys.Nemesys'],
+        'icon_resources': [(1, '.\\icons\\nemesys.ico')],
+        'cmdline_style': 'pywin32',
+    }],
+    windows=[{
+        'script': os.path.join('nemesys', 'login.py'),
+        'uac_info': 'requireAdministrator',
+        'icon_resources': [(1, '.\\icons\\nemesys.ico')],
+    }],
+    version_info={
+        'version': get_version(),
+        'company_name': 'Fondazione Ugo Bordoni',
+        'copyright': '(c)2010-2017 Fondazione Ugo Bordoni',
     },
-    version=get_version(),
-    service=[nemesys_service],
-    windows=[
-        {'name': 'Nemesys login',
-         'script': os.path.join('nemesys', 'login.py'),
-         'uac_info': 'requireAdministrator',
-         'icon_resources': [(1, '.\\icons\\nemesys.ico')]}
-    ],
+    options={
+        'packages': ['encodings', 'common', 'nemesys', 'charset_normalizer'],
+        'includes': ['_cffi_backend'],
+        'optimize': 2,
+    },
 )
